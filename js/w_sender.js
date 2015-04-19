d8aefc2e966a0482acd493a014fd269766efc910
Владимир <svr93@i.ua>
2015-04-20 01:34:39 +0400

    w_sender.js___ver2.0

--- js/w_sender.js
+++ js/w_sender.js
@@ -1,53 +1,42 @@
 /* to do:
   1) add try-catch blocks instead of errCallback calling;
+  2) repeat a test for memory leaks (Chrome, Firefox)
 */
 
 onmessage = function(evt) {
   'use strict';
 
-  var DELAY_TIME = 2500;
+  var DELAY_TIME = 500;
 
   var xhr = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function() {
-
-    if (this.readyState != 4 && this.status != 200) {
-      return;
-    }
-
-    if (this.readyState == 4 && this.status != 200) {
-      return errCallback('Ошибка при получении координат');
-    }
-
-    if (!this.responseText) {
-      return;
-    }
-
-    var res = JSON.parse(this.responseText);
-
-    if (res.error) {
-      var msg = res.error + '; ' + res.explanation;
-      return errCallback(msg);
-    }
-
-    var resText = this.responseText.
-                    replace(/,/g, ',\n').
-                    replace(/\[/g, '[\n\n');
-
-    setTimeout(function() {
-
-      postMessage({
-        status: 0,
-        res: resText
-      });
-
-    }, DELAY_TIME);
-
-  };
-
-  xhr.open('GET', '/info');
+  xhr.open('GET', '/info', false);
 
   xhr.send();
+
+  if (xhr.readyState == 4 && xhr.status != 200) {
+    return errCallback('Ошибка при получении координат');
+  }
+
+  var res = JSON.parse(xhr.responseText);
+
+  if (res.error) {
+    var msg = res.error + '; ' + res.explanation;
+    return errCallback(msg);
+  }
+
+  var resText = xhr.responseText.
+                  replace(/,/g, ',\n').
+                  replace(/\[/g, '[\n\n');
+
+  setTimeout(function() {
+
+    postMessage({
+      status: 0,
+      res: resText
+    });
+
+  }, DELAY_TIME);
 
   function errCallback(msg) {
 
