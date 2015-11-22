c3dce89faa89fe648f7d25f9233be108e145c75f
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-11-22 17:52:13 +0300

    add_Cesium.js_script

--- js/module/cesium-base.js
+++ js/module/cesium-base.js
@@ -1,11 +1,14 @@
 'use strict';
 
 import { ng } from 'js/module/wrapper/angular.js';
+import { cs } from 'js/module/wrapper/cesium.js';
 
 /**
  * Cesium part entry point.
  */
 export function initialize() {
 
-    console.log(`initialization successful; ng version: ${ ng.version.full }`);
+    console.log(
+        `ng version: ${ ng.version.full }, cs version: ${ cs.VERSION }`
+    );
 }
---
+++ js/module/wrapper/cesium.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * Reference on Cesium.
+ * @type {Object}
+ */
+export const cs = window.Cesium;
